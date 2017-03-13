c45d2ec845ade061bbd76818ae1b0b2ee577da56
JesusLoovesYou <ivanangelov87@gmail.com>
2017-03-13 15:39:39 +0200

    Add Data and Web skeleton

---
+++ CarAdvertsSystem/CarAdverts.Data/CarAdvertsSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CarAdverts.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace CarAdverts.Data
+{
+    public class CarAdvertsSystemDbContext : IdentityDbContext<User>
+    {
+        public CarAdvertsSystemDbContext()
+            : base("CarAdvertsSystemDb")
+        {
+        }
+
+        public static CarAdvertsSystemDbContext Create()
+        {
+            return new CarAdvertsSystemDbContext();
+        }
+    }
+}
