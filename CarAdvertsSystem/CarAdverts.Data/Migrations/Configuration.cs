ac9c0bfdf184bceffe40c36837fa665f333f0770
JesusLoovesYou <ivanangelov87@gmail.com>
2017-03-28 01:04:03 +0300

    Remove unused Test Controller

--- CarAdvertsSystem/CarAdverts.Data/Migrations/Configuration.cs
+++ CarAdvertsSystem/CarAdverts.Data/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 using System.Data.Entity.Migrations;
 using System.Linq;
 using CarAdverts.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace CarAdverts.Data.Migrations
 {
@@ -14,100 +16,80 @@ namespace CarAdverts.Data.Migrations
 
         protected override void Seed(CarAdvertsSystemDbContext context)
         {
-            //if (context.Roles.Count() == 0)
+            if (!context.Roles.Any(r => r.Name == "Admin"))
             {
-                //context.Roles.Add(new IdentityRole("admin"));
-                //context.Roles.Add(new IdentityRole("user"));
-                //context.SaveChanges();
+                this.AddUserAndRole(context);
+            }
 
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddOrUpdate(c => c.Id,
+                    new Category { Id = 0, Name = "Bus" },
+                    new Category { Id = 1, Name = "Caravan" },
+                    new Category { Id = 2, Name = "Car" },
+                    new Category { Id = 3, Name = "Motorbike" },
+                    new Category { Id = 5, Name = "SUV" }
+                );
+            }
 
-                //var adminRole = new IdentityUserRole();
-                //adminRole.RoleId = context.Roles.FirstOrDefault(r => r.Name == "admin").Id;
-                //context.SaveChanges();
+            if (!context.Manufacturers.Any())
+            {
+                context.Manufacturers.AddOrUpdate(m => m.Id,
+                    new Manufacturer { Id = 1, Name = "Audi" },
+                    new Manufacturer { Id = 2, Name = "Peugeot" },
+                    new Manufacturer { Id = 3, Name = "KIA" },
+                    new Manufacturer { Id = 4, Name = "Dacia" },
+                    new Manufacturer { Id = 5, Name = "Lamborghini" },
+                    new Manufacturer { Id = 6, Name = "BMV" }
+                );
+            }
 
-                //var userRole = new IdentityUserRole();
-                //userRole.RoleId = context.Roles.FirstOrDefault(r => r.Name == "user").Id;
-                //context.SaveChanges();
+            if (!context.VehicleModels.Any())
+            {
+                context.VehicleModels.AddOrUpdate(v => v.Id,
+                    new VehicleModel { Id = 1, Name = "A4", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 2, Name = "A6", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 3, Name = "A8", CategoryId = 2, ManufacturerId = 1 },
+                    new VehicleModel { Id = 4, Name = "100", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 5, Name = "S8", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 6, Name = "XS", CategoryId = 2, ManufacturerId = 2 },
+                    new VehicleModel { Id = 7, Name = "TT", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 8, Name = "Uou", CategoryId = 2, ManufacturerId = 3 },
+                    new VehicleModel { Id = 9, Name = "Test", CategoryId = 1, ManufacturerId = 1 },
+                    new VehicleModel { Id = 10, Name = "80", CategoryId = 1, ManufacturerId = 1 }
+                );
+            }
 
-                //var adminUser = context.Users.First(u => u.Email == "admin@abv.bg");
-                //var regularUser = context.Users.First(x => x.Email == "chuk@.abv.bg");
-
-                //if (adminUser != null && regularUser != null)
-                {
-                    //regularUser.Roles.Add(userRole);
-                    //context.SaveChanges();
-                    //adminUser.Roles.Clear();
-                    //adminUser.Roles.Add(adminRole);
-                    //context.SaveChanges();
-                }
-
-                ///////////////////////////////////////////////////////
-
-                if (!context.Categories.Any())
-                {
-                    context.Categories.AddOrUpdate(c => c.Id,
-                        new Category { Id = 0, Name = "Bus" },
-                        new Category { Id = 1, Name = "Caravan" },
-                        new Category { Id = 2, Name = "Car" },
-                        new Category { Id = 3, Name = "Motorbike" },
-                        new Category { Id = 5, Name = "SUV" }
-                    );
-                }
-
-                if (!context.Manufacturers.Any())
-                {
-                    context.Manufacturers.AddOrUpdate(m => m.Id,
-                        new Manufacturer { Id = 1, Name = "Audi" },
-                        new Manufacturer { Id = 2, Name = "Peugeot" },
-                        new Manufacturer { Id = 3, Name = "KIA" },
-                        new Manufacturer { Id = 4, Name = "Dacia" },
-                        new Manufacturer { Id = 5, Name = "Lamborghini" },
-                        new Manufacturer { Id = 6, Name = "BMV" }
-                    );
-                }
-
-                if (!context.VehicleModels.Any())
-                {
-                    context.VehicleModels.AddOrUpdate(v => v.Id,
-                        new VehicleModel { Id = 1, Name = "A4", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 2, Name = "A6", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 3, Name = "A8", CategoryId = 2, ManufacturerId = 1 },
-                        new VehicleModel { Id = 4, Name = "100", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 5, Name = "S8", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 6, Name = "XS", CategoryId = 2, ManufacturerId = 2 },
-                        new VehicleModel { Id = 7, Name = "TT", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 8, Name = "Uou", CategoryId = 2, ManufacturerId = 3 },
-                        new VehicleModel { Id = 9, Name = "Test", CategoryId = 1, ManufacturerId = 1 },
-                        new VehicleModel { Id = 10, Name = "80", CategoryId = 1, ManufacturerId = 1 }
-                    );
-                }
-
-                if (!context.Cities.Any())
-                {
-                    context.Cities.AddOrUpdate(c => c.Id,
-                        new City { Id = 1, Name = "Sofia" },
-                        new City { Id = 2, Name = "Dupnica" },
-                        new City { Id = 3, Name = "Tyrnovo" },
-                        new City { Id = 4, Name = "Haskovo" }
-                    );
-                }
-
-                //if (!context.Adverts.Any())
-                //{
-                //    context.Adverts.AddOrUpdate(a => a.Id,
-                //        new Advert()
-                //        {
-                //            Title = "adv1",
-                //            CityId = 1,
-                //            VehicleModelId = 1,
-                //            DistanceCoverage = 500,
-                //            Price = 100,
-                //            Power = 100,
-                //            Description = "neznam iide"
-                            
-                //        });
-                //}
+            if (!context.Cities.Any())
+            {
+                context.Cities.AddOrUpdate(c => c.Id,
+                    new City { Id = 1, Name = "Sofia" },
+                    new City { Id = 2, Name = "Dupnica" },
+                    new City { Id = 3, Name = "Tyrnovo" },
+                    new City { Id = 4, Name = "Haskovo" }
+                );
             }
         }
+
+        private bool AddUserAndRole(CarAdvertsSystemDbContext context)
+        {
+            var user = new User()
+            {
+                UserName = "admin@abv.bg",
+            };
+
+            var roleMenager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            IdentityResult identityResult = roleMenager.Create(new IdentityRole("Admin"));
+            var userManager = new UserManager<User>(new UserStore<User>(context));
+            identityResult = userManager.Create(user, "123456");
+            if (identityResult.Succeeded == false)
+            {
+                return identityResult.Succeeded;
+            }
+
+            identityResult = userManager.AddToRole(user.Id, "Admin");
+            return identityResult.Succeeded;
+        }
+
     }
 }
